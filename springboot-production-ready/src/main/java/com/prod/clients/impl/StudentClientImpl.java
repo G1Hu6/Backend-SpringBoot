a893e093e22546d2b9d83fa170bf3359bfff6565
G1Hu6 <r1ruru219@gmail.com>
2025-01-01 15:59:24 +0530

    adding Slf loging- logger, formater and handler

--- springboot-production-ready/src/main/java/com/prod/clients/impl/StudentClientImpl.java
+++ springboot-production-ready/src/main/java/com/prod/clients/impl/StudentClientImpl.java
@@ -5,6 +5,8 @@ import com.prod.clients.StudentClient;
 import com.prod.dto.StudentDto;
 import com.prod.exceptions.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -18,18 +20,32 @@ import java.util.List;
 public class StudentClientImpl implements StudentClient {
 
     private final RestClient restClient;
+    Logger log = LoggerFactory.getLogger(StudentClientImpl.class);
+
 
     @Override
     public List<StudentDto> getAllStudent(){
+//        log.error("Error Log");
+//        log.warn("Warn Log");
+//        log.info("Executing getAllStudents Method...");
+//        log.debug("Debug Log");
+//        log.trace("Trace Log");
+
         try{
+            log.info("Executing getAllStudents Method...");
             ApiResponse<List<StudentDto>> allStudents = restClient.get()
                     .uri("db/students")
                     .retrieve()
+                    .onStatus(HttpStatusCode::is4xxClientError,(request, response) -> {
+                        log.error("4xxERROR occurred at getAllStudent , Error : {}", new String(response.getBody().readAllBytes()));
+                        throw new ResourceNotFoundException("Students Not found in database");
+                    })
                     .body(new ParameterizedTypeReference<>(){});
 
-
+            log.info("SUCCESS in getAllStudents Method...");
             return allStudents.getData();
         } catch (Exception e) {
+            log.error("Client Error getAllStudent Method...");
             throw new RuntimeException(e);
         }
     }
@@ -38,17 +54,22 @@ public class StudentClientImpl implements StudentClient {
     @Override
     public StudentDto getStudentById(Long id) {
         try{
+            log.info("Executing getStudentById Method...");
             ApiResponse<StudentDto> studentDtoApiResponse = restClient.get()
                     .uri("db/students/{id}", id)
                     .retrieve()
                     .onStatus(HttpStatusCode::is4xxClientError,(request, response) -> {
-                        System.out.println(new String(response.getBody().readAllBytes()));
+                        log.error("4xxERROR occurred at getStudentById , Error : {}",new String(response.getBody().readAllBytes()));
                         throw new ResourceNotFoundException("Student Not Found wit id + " + id);
                     })
                     .body(new ParameterizedTypeReference<ApiResponse<StudentDto>>() {
                     });
+
+            log.info("SUCCESS in getStudentById Method...");
+            log.trace("Returned Student : {}",studentDtoApiResponse.getData());
             return studentDtoApiResponse.getData();
         } catch (Exception e) {
+            log.error("Client Error Occurred getStudentById ...");
             throw new RuntimeException(e);
         }
     }
@@ -56,18 +77,22 @@ public class StudentClientImpl implements StudentClient {
     @Override
     public StudentDto createStudent(StudentDto studentDto) {
         try {
+            log.info("Executing createStudent Method...");
             ResponseEntity<ApiResponse<StudentDto>> apiResponseResponseEntity = restClient.post()
                     .uri("db/students")
                     .body(studentDto)
                     .retrieve()
                     .onStatus(HttpStatusCode::is4xxClientError,(request, response) -> {
-                        System.out.println(new String(response.getBody().readAllBytes()));
+                        log.error("4xxERROR occurred at createStudent , Error : {}",new String(response.getBody().readAllBytes()));
                         throw new ResourceNotFoundException("Could Not Create the Student");
                     }).toEntity(new ParameterizedTypeReference<ApiResponse<StudentDto>>() {
                     });
 
+            log.info("SUCCESS in createStudent Method...");
+            log.trace("Saved Student : {}", apiResponseResponseEntity.getBody().getData());
             return apiResponseResponseEntity.getBody().getData();
         } catch (Exception e) {
+            log.error("Client Error Occurred createStudent...");
             throw new RuntimeException(e);
         }
     }
