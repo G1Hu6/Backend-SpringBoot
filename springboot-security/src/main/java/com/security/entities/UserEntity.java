1e3b94608620c7717c9448e643cab0599184aca2
G1Hu6 <r1ruru219@gmail.com>
2025-01-05 19:01:03 +0530

    Creating and verification of JWT tokens

--- springboot-security/src/main/java/com/security/entities/UserEntity.java
+++ springboot-security/src/main/java/com/security/entities/UserEntity.java
@@ -1,10 +1,7 @@
 package com.security.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -16,6 +13,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Builder
 public class UserEntity implements UserDetails {
 
     @Id
--- springboot-security/src/test/java/com/security/SpringbootSecurityApplicationTests.java
+++ springboot-security/src/test/java/com/security/SpringbootSecurityApplicationTests.java
@@ -1,13 +1,30 @@
 package com.security;
 
+import com.security.entities.UserEntity;
+import com.security.services.JWTService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.User;
 
 @SpringBootTest
 class SpringbootSecurityApplicationTests {
 
+	@Autowired
+	private JWTService jwtService;
+
 	@Test
 	void contextLoads() {
+		UserEntity userEntity = UserEntity.builder()
+				.id(2L)
+				.email("user@gmail.com")
+				.password("243wrw")
+				.build();
+		String token = jwtService.generateToken(userEntity);
+		System.out.println(token);
+
+		Long id = jwtService.getUserIdFromToken(token);
+		System.out.println(id);
 	}
 
 }
