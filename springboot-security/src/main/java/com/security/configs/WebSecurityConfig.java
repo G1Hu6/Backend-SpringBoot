cfd6be498cbbdeb76e639be12aa3ffd809e8158f
G1Hu6 <r1ruru219@gmail.com>
2025-01-05 18:08:25 +0530

    SecurityFilterChain configuration with WebConfig

---
+++ springboot-security/src/main/java/com/security/configs/WebSecurityConfig.java
@@ -0,0 +1,61 @@
+package com.security.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception{
+        return httpSecurity
+                //.formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.loginPage("/new_login.html"))
+                .authorizeHttpRequests(auth -> auth
+                                //.anyRequest().authenticated()
+                                .requestMatchers("/posts").permitAll()
+                                .requestMatchers("/posts/**").hasAnyRole("ADMIN")
+                                //.requestMatchers("/posts/**").permitAll()
+                                //.requestMatchers("/posts").permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionConfig-> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+
+    }
+
+    @Bean
+    UserDetailsService muInMemoryUserDetailsService(){
+        UserDetails normalUser = User.builder()
+                .username("pranav")
+                .password(passwordEncoder().encode("1234"))
+                .roles("USER")
+                .build();
+
+        UserDetails adminUser = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(normalUser, adminUser);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
