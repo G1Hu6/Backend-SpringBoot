2fcb6dfc084786fc5192695c153f51759f25705f
G1Hu6 <r1ruru219@gmail.com>
2025-01-06 20:51:36 +0530

    Handling Exceptions in Spring Security - AuthenticationException and JwtException

--- springboot-security/src/main/java/com/security/advices/GlobalExceptionHandler.java
+++ springboot-security/src/main/java/com/security/advices/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.security.advices;
 
 import com.security.exceptions.ResourceNotFoundException;
+import io.jsonwebtoken.JwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,4 +19,29 @@ public class GlobalExceptionHandler {
                 .build();
         return new ResponseEntity<>(apiError,HttpStatus.NOT_FOUND);
     }
+
+    // Handle AuthenticationException
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ApiError> handleAuthenticationException(AuthenticationException exception){
+        ApiError apiError = ApiError.builder()
+                .httpStatus(HttpStatus.UNAUTHORIZED)
+                .message(exception.getMessage())
+                .build();
+        return new ResponseEntity<>(apiError,HttpStatus.UNAUTHORIZED);
+    }
+
+    // We can not handle JwtException(ExpiredJwtException and all child) using GlobalExceptionHandler
+    // because it can't handle exceptions that occurs before running Dispatcher
+    // Servlet so use HandlerExceptionResolver
+
+    //Handle JwtException
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<ApiError> handleJwtException(JwtException exception){
+        ApiError apiError = ApiError.builder()
+                .httpStatus(HttpStatus.UNAUTHORIZED)
+                .message(exception.getMessage())
+                .build();
+        return new ResponseEntity<>(apiError,HttpStatus.UNAUTHORIZED);
+    }
+
 }
