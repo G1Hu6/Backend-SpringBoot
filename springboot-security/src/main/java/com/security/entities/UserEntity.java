e8c699d4c9cb9eda25530707941b54898471f613
G1Hu6 <r1ruru219@gmail.com>
2025-01-09 21:59:23 +0530

    Granular Authorization with custom mapping of roles and permissions

--- springboot-security/src/main/java/com/security/entities/UserEntity.java
+++ springboot-security/src/main/java/com/security/entities/UserEntity.java
@@ -2,6 +2,7 @@ package com.security.entities;
 
 import com.security.entities.enums.Permission;
 import com.security.entities.enums.Role;
+import com.security.utils.RolePermissionMappings;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
@@ -9,6 +10,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -33,18 +35,28 @@ public class UserEntity implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
-    @ElementCollection(fetch = FetchType.EAGER)
-    @Enumerated(EnumType.STRING)
-    private Set<Permission> permissions;
+//    @ElementCollection(fetch = FetchType.EAGER)
+//    @Enumerated(EnumType.STRING)
+//    private Set<Permission> permissions;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        Set<SimpleGrantedAuthority> authorities = roles.stream()
-                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
-                .collect(Collectors.toSet());
-
-        permissions
-                .forEach(permission -> authorities.add(new SimpleGrantedAuthority(permission.name())));
+//        Set<SimpleGrantedAuthority> authorities = roles.stream()
+//                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
+//                .collect(Collectors.toSet());
+//
+//        permissions
+//                .forEach(permission -> authorities.add(new SimpleGrantedAuthority(permission.name())));
+//
+//        return authorities;
+        Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+        roles.forEach(
+                role -> {
+                    Set<SimpleGrantedAuthority> permissions = RolePermissionMappings.getAuthoritiesForRole(role);
+                    authorities.addAll(permissions);
+                    authorities.add(new SimpleGrantedAuthority("ROLE_"+role.name()));
+                }
+        );
 
         return authorities;
     }
